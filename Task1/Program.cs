3b39dd6f11498f49983a1fae8d4eb2d323436408
kate <01redcat@mail.ru>
2022-07-10 17:40:43 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число квадратный корень указанного числа ");
+int b = Convert.ToInt32(Console.ReadLine());
+int res = b*b;
+if(res==a)
+    Console.WriteLine($"Да, {b} является квадратным корнем числа {a}");
+else Console.WriteLine($"Нет, {b} не является квадратным корнем числа {a}");       
+
+      
