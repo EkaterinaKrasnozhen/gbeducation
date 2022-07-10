c0d79f8c063c47f1eccc1e231a50a89f525d72be
kate <01redcat@mail.ru>
2022-07-10 19:54:52 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = -(n);
+while (count <= n) 
+{
+    Console.Write($"{count}");
+    count++;
+}     
+ 
