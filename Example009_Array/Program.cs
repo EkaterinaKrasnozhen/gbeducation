92aa34cdd417fc6559b30a73ae1bdc42271f79c4
kate <01redcat@mail.ru>
2022-07-12 12:07:47 +0300

    example009_array

---
+++ Example009_Array/Program.cs
@@ -0,0 +1,14 @@
+﻿int [] array = {9,8,22,6,5,22};
+int n = array.Length;
+int find = 22;
+int index = 0;
+while (index<n) 
+{
+  if (array[index] == find)
+   {
+    Console.WriteLine(index);
+    //break; если нужно найти только первый элемент, совпадающий с find - написать break, 
+    //если нет  - покажет все индексы, совпадающие с числом find
+   }
+   index++;
+}
