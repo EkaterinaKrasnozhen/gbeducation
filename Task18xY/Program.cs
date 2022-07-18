49132a6d43d4e425d72b07ddb04730b488f55043
kate <01redcat@mail.ru>
2022-07-18 12:16:51 +0300

    task18 added

--- Task18xY/Program.cs
+++ Task18xY/Program.cs
@@ -11,5 +11,15 @@ string Quarter(int numberQuart)
     if (numberQuart == 4) return "x > 0 и y < 0";
     return "Введена некорректная четверть";
 }
+//int find = Console.ReadLine();
+//string Quarter(string numberQuart)
+//{
+ //   if (numberQuart == "1") return "x > 0 и y > 0";
+ //   if (numberQuart == "2") return "x < 0 и y > 0";
+ //   if (numberQuart == "3") return "x < 0 и y < 0";
+ //   if (numberQuart == "4") return "x > 0 и y < 0";
+ //   return "Введена некорректная четверть";
+//}
+//можно использовать значение "строка" без перевода в число, тогда значение присваиваем в ""
 string result = Quarter(find);
 Console.WriteLine(result);
