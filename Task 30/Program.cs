e960e8360f3d7cf6f06ad9e92c2923cf6bdae3ae
kate <01redcat@mail.ru>
2022-08-09 16:09:55 +0300

    task upgrade

--- Task 30/Program.cs
+++ Task 30/Program.cs
@@ -5,9 +5,10 @@
 
 void FillArray(int[] numbers)
 {
+    Random rnd = new Random();
     for (int i = 0; i < numbers.Length; i++)
     {
-        numbers[i] = new Random().Next(0, 2);
+        numbers[i] = rnd.Next(0, 2);
     }
 }
 void PrintArray(int[] col)
@@ -15,11 +16,12 @@ void PrintArray(int[] col)
     for (int i = 0; i < col.Length; i++)
     {
         Console.Write(col[i]);
-        if (i == 0) Console.Write ("[");
-        if (i == 7) Console.Write ("]");
-        if (i == col.Length-1) Console.Write (",");
+        if (i != col.Length-1) 
+        Console.Write(",");
     }
 }
 int [] array = new int [8];
 FillArray(array);
+Console.Write("[");
 PrintArray(array);
+Console.Write("]");
