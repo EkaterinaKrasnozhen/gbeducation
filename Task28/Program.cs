fa649dba5137bb0bef0c3da8ae5c42f8c8e06dee
kate <01redcat@mail.ru>
2022-08-09 13:06:22 +0300

    Task 28 method for

---
+++ Task28/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 28: Напишите программу, которая
+//принимает на вход число N и выдаёт
+//произведение чисел от 1 до N.
+//4 -> 24
+//5 -> 120
+Console.WriteLine("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int Multi(int number)
+{
+    int count = 1;
+    for (int i = 1; i <= number; i++)
+    {
+        count = count * i;
+    }
+    return count;
+}
+if (n == 0) Console.Write($"{n}-> 0");
+else Console.Write($"{n} ->  {Multi(n)}");
