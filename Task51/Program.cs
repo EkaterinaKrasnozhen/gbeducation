7216ae10aa687219cd44f1cd441186a789f4ed0e
kate <01redcat@mail.ru>
2022-08-15 17:22:24 +0300

    Task 51 сумма по главной диагонали

---
+++ Task51/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Задача 51: Задайте двумерный массив. Найдите сумму
+элементов, находящихся на главной диагонали (с индексами
+(0,0); (1;1) и т.д. */
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 5);
+        }
+    }
+    return matrix;
+}
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+
+
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j],3}|");
+            else Console.Write($"{matr[i, j],3}]");
+        }
+        Console.WriteLine();
+    }
+}
+int SumDiagonal(int[,] matr)
+{
+    int sum = 0;
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                Console.Write(matr[i, j]);
+                sum = sum + matr[i,j];
+            }
+
+
+        }
+    }
+    return sum;
+}
+int[,] matrixResult = GetMatrix(3, 4);
+PrintMatrix(matrixResult);
+int sumResult = SumDiagonal(matrixResult);
+Console.Write($"   {sumResult}");
