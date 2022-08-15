a2128a02b13457ef2d4656b9bb1f50f2af7c542a
kate <01redcat@mail.ru>
2022-08-15 15:18:40 +0300

    Example Fibonacci Recurtion

---
+++ Example_ Fibonacci_ Recurtion/Program.cs
@@ -0,0 +1,10 @@
+﻿// Числа Fibonacci
+//f(1) = 1
+//f(2) = 1
+int Fibonacci(int n) 
+{
+    if (n ==1 || n ==2) return 1;
+    else return Fibonacci(n-1)+Fibonacci(n-2);
+}
+
+Console.WriteLine(Fibonacci(n));
