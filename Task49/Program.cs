45fe5f304044fb6ab62b72dbc203bc8b96a12263
kate <01redcat@mail.ru>
2022-08-15 17:02:19 +0300

    Task 49 возвести в квадрат четные индексы матрицы

---
+++ Task49/Program.cs
@@ -0,0 +1,51 @@
+﻿/* //\Задача 49: Задайте двумерный массив. Найдите элементы, у
+которых оба индекса чётные, и замените эти элементы на их
+квадраты.
+ */
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(1, 5);
+        }
+    }
+    return matrix;
+}
+int[,] matResult = GetMatrix(4, 4);
+int[,] MatrEven(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0) matr[i, j] = matr[i, j] * matr[i, j];
+        }
+
+
+    }
+    return matr;
+}
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+
+
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            if (j == 0) Console.Write("[");
+            if (j < matr.GetLength(1) - 1) Console.Write($"{matr[i, j],3}|");
+            else Console.Write($"{matr[i, j],3}]");
+        }
+        Console.WriteLine();
+    }
+}
+PrintMatrix(matResult);
+int[,] matrEven = MatrEven(matResult);
+Console.WriteLine();
+PrintMatrix(matrEven);
